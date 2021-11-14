63baa3e77e82a214c9f2387bf86c32ae0e5b59e4
Hxnzo <hanzalahp20@gmail.com>
2021-11-14 15:46:10 -0500

    hanzalah about us commit 1

--- src/JavaScript/About.js
+++ src/JavaScript/About.js
@@ -4,23 +4,54 @@ import '../Styles/About.css';
 
 const About = () => {
     return (
-        <div className="Test">
-            <h1>ABOUT US BOIII</h1>
-            <h1>ABOUT US BOIII</h1>
-            <h1>ABOUT US BOIII</h1>
-            <h1>ABOUT US BOIII</h1>
-            <h1>ABOUT US BOIII</h1>
-            <h1>ABOUT US BOIII</h1>
-            <h1>ABOUT US BOIII</h1>
-            <h1>ABOUT US BOIII</h1>
-            <h1>ABOUT US BOIII</h1>
-            <h1>ABOUT US BOIII</h1>
-            <h1>ABOUT US BOIII</h1>
-            <h1>ABOUT US BOIII</h1>
-            <h1>ABOUT US BOIII</h1>
+        <div>
+            <div className="about">
+                <h1>About Us</h1>
+            </div>
+
+            <div className="img-section">
+                <p>Display img</p>
+            </div>
+
+            <div className="description">
+                <p>Company description</p>
+            </div>
+
+            <div className="staff">
+                <h1>Staff Descriptions</h1>
+            </div>
+
+            {/*Staff descriptions below*/ }
+
+            <div className="huzaifa-img">
+                <p>huzaifa img</p>
+            </div>
+            <div className="huzaifa">
+                <p>my name is huzaifa</p>
+            </div>
+
+            <div className="hanzalah-img">
+                <p>hanzalah image</p>
+            </div>
+            <div className="hanzalah">
+                <p>my name is hanzalah</p>
+            </div>
+
+            <div className="zubair">
+                <p>my name is huzaifa</p>
+            </div>
+            <div className="zubair-img">
+                <p>my name is huzaifa</p>
+            </div>
+
+            <div className="ziyad">
+                <p>my name is huzaifa</p>
+            </div>
+            <div className="ziyad-img">
+                <p>my name is huzaifa</p>
+            </div>
 
         </div>
-
     );
 }
 
