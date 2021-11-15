59b6f9bf4de87c6db8b77dbe74eca253eecc01d4
huzaifazia17 <ziahuzaifa123@gmail.com>
2021-11-14 23:24:03 -0500

    finished howto section

--- src/JavaScript/Home.js
+++ src/JavaScript/Home.js
@@ -2,12 +2,14 @@
 import React, { Component } from 'react';
 import '../Styles/Home.css';
 import Hero from './hero';
+import HowTo from './HowTo';
 
 
 const Home = () => {
     return (
         <div>
             <Hero></Hero>
+            <HowTo />
             <h1>ya</h1>
             <h1>ya</h1>
             <h1>ya</h1>
---
+++ src/JavaScript/HowTo.js
@@ -0,0 +1,25 @@
+import '../Styles/howto.css';
+import img from '../images/howtoimg.png';
+
+const HowTo = () => {
+    return (
+        <div className="howto-container">
+            <h1>How To Shop- Simple 1, 2 & 3 </h1>
+            <div>
+            </div>
+            <div>
+                <ul className="howto-list">
+                    <li><img src={img}></img> </li>
+                    <li className="wordlist"><i class="fas fa-scroll"></i> Scroll down and choose your items from our wide selection!
+                        <br></br><br></br><i class="fas fa-shopping-cart"></i> Choose how many you want and add them to your cart!
+                        <br></br><br></br><i class="fas fa-money-check-alt"></i> Add some more items and then go to your cart and checkout!
+                    </li>
+                </ul>
+            </div>
+
+        </div >
+
+    )
+}
+
+export default HowTo;
