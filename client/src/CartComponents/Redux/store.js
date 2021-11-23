203e87e4be873fbd8d94481ab6e5ba1e5cd11280
zubxxr <zubxxr@gmail.com>
2021-11-23 16:33:34 -0500

    changes

--- client/src/CartComponents/Redux/store.js
+++ client/src/CartComponents/Redux/store.js
@@ -1,7 +1,6 @@
 import { createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import rootReducer from "./rootReducer";
-
+import rootReducer from "./rootreducer";
 const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
